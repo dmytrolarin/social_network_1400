02a37cac7162d4dbf9d79610d36c024383709613
Dmytro Larin <dmytrolarin.development@gmail.com>
2025-12-14 14:31:00 +0100

    Added unique user validation

--- social_network/user_app/views.py
+++ social_network/user_app/views.py
@@ -1,15 +1,20 @@
 from django.shortcuts import render
 from django.contrib.auth.models import User
+from django.http import HttpRequest
+from django.db.utils import IntegrityError
 
 
-def render_registration(request):
+def render_registration(request: HttpRequest):
     error = ""
     if request.method == "POST":
         username = request.POST.get("username")
         password = request.POST.get("password")
         password_confirm = request.POST.get("password_confirm")
         if password == password_confirm:
-            User.objects.create_user(username=username, password=password)
+            try:
+                User.objects.create_user(username=username, password=password)
+            except IntegrityError:
+                error = "Такий користовач вже існує"
         else:
             error = "Паролі не співпадають"
     return render(request, 'user_app/registration.html', context={"error": error})
