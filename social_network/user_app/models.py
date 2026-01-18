bb967188c76d350b236c25fb2a171d29dc0cbe75
Dmytro Larin <dmytrolarin.development@gmail.com>
2026-01-18 14:46:22 +0100

    added Profile model

--- social_network/user_app/models.py
+++ social_network/user_app/models.py
@@ -1,3 +1,6 @@
 from django.db import models
-
+from phonenumber_field.modelfields import PhoneNumberField
 # Create your models here.
+class Profile(models.Model):
+    about_me = models.TextField()
+    phone_number = PhoneNumberField()
