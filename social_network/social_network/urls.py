4d6261c49756478d4990d205a1d44cd531a9426a
Dmytro Larin <dmytrolarin.development@gmail.com>
2026-01-25 14:37:06 +0100

    Media files

--- social_network/social_network/urls.py
+++ social_network/social_network/urls.py
@@ -16,8 +16,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
+from .settings import DEBUG, MEDIA_URL, MEDIA_ROOT
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('user/', include('user_app.urls'))
 ]
+
+if DEBUG:
+    urlpatterns += static(MEDIA_URL, document_root=MEDIA_ROOT)
--- social_network/user_app/models.py
+++ social_network/user_app/models.py
@@ -1,6 +1,14 @@
 from django.db import models
 from phonenumber_field.modelfields import PhoneNumberField
-# Create your models here.
+from django.contrib.auth.models import User
+
 class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, null=True)
     about_me = models.TextField()
     phone_number = PhoneNumberField()
+    avatar = models.ImageField(upload_to='images/avatars/', null=True)
+    
+    # def __str__(self):
+    #     return self.user
+
+    
